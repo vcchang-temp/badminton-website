ae440d6e97e45cec2aa6535baeea828a642f3a0d
vcchang <vcchang@users.noreply.github.com>
2017-05-22 13:33:20 -0700

    First commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+/* 
+
+This file contains code for the jQuery calendar and scripting for the animations
+
+*/
+
+/* TODO's
+
+1. 	Animate badminton player icon: slide, fade-in
+2. 	Smoothen transition from main page to search section when 
+	"Find places to play" button is pressed
+3. 	Create smooth transition from search section to badminton-place
+	suggestions when "Search" is pressed
+4. 	Potential TODO: create animation of badminton-place cards
+	reorganizing when new search is performed
+
+*/
+
+
+// calendar
+
+$("#calendar").datepicker();
+
+
